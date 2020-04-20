4144d787f54ab3398e102278e78016a4163fb641
Nicolas Vuaille <nicolas.vuaille+git@gmail.com>
2020-04-20 19:52:00 +0200

    Mise à jour du script

--- nwc2musicxml.py
+++ nwc2musicxml.py
@@ -20,27 +20,30 @@ original_files.sort()
 dest_dir = os.path.abspath(sys.argv[2])
 
 failed_files = []
-for f in original_files:
-    print(f)
+for current_file in original_files:
+    print(f"processing {current_file}", end='\r', flush=True)
     try:
-        f = os.path.abspath(f)
+        current_file = os.path.abspath(current_file)
         instrument._ID = 0
-        c = converter.parse(f)
+        c = converter.parse(current_file)
     except Exception:
-        failed_files.append(f)
-        print(f"fail to parse {f}")
+        failed_files.append(current_file)
+        print(f"fail to parse {current_file}")
         continue
     try:
-        filename = f.split('/')[-1]
+        filename = current_file.split('/')[-1]
         new_file = dest_dir + '/'
         new_file += filename.replace("nwc", "musicxml")
         c.write(fmt="musicxml", fp=new_file)
     except Exception:
-        failed_files.append(f)
+        failed_files.append(current_file)
+        print('')
         print(f"fail to write {new_file}")
         continue
 
-print("finished !")
+    sys.stdout.write("\033[K")
+
 if len(failed_files) > 0:
-    print("failed to convert:")
-    print(failed_files)
+    print('terminated with errors')
+
+print("finished")
