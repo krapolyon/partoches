555d00e03986180f68a73d82325ff5b67ad2751e
Nicolas Vuaille <nicolas.vuaille@gmail.com>
2023-08-07 19:28:37 +0200

    Refacto script
    * use argparse
    * use pathlib
    * remove dead code

--- nwc2musicxml.py
+++ nwc2musicxml.py
@@ -1,3 +1,4 @@
+import argparse
 import contextlib
 import datetime
 import glob
@@ -12,88 +13,92 @@ from music21 import instrument
 from music21 import Music21Exception
 
 # -----------------
-# Sanitize arguments
-if len(sys.argv) != 3:
-    print("""
-usage : nwc2musicxml source dest
-
-source : the files to parse. Should be one string describing the input.
-         Can be a directory. Can contains regexp (is passed to glob.glob())
-
-dest : a directory where to write the files.
-""")
-    sys.exit(1)
-
-# -----------------
-# Init log
-log_file_name = 'converter.log'
-with open(log_file_name, 'w') as logger:
-    logger.write('nwc to musicxml converter\n')
-    logger.write(str(datetime.datetime.now()) + '\n\n')
-
-original_files = glob.glob(sys.argv[1])
-original_files.sort()
-dest_dir = os.path.abspath(sys.argv[2])
+def initLog(file_name):
+    with open(log_file_name, 'w') as logger:
+        logger.write('nwc to musicxml converter\n')
+        logger.write(str(datetime.datetime.now()) + '\n\n')
 
 # -----------------
 def printAndLog(error_type, file_name, error_string):
-    global log_file_name
+    log_file_name = 'converter.log'
+    log_path = Path(log_file_name)
+    if not log_path.exists():
+        initLog(log_file_name)
+
     with open(log_file_name, 'a') as logger:
         logger.write(f'{error_type} error:')
         logger.write(f'\n  {file_name}')
         logger.write(f'\n  {error_string}\n\n')
 
-    colored_error = '\033[31;1merror\033[39;22m'
-    print(f'{colored_error} while {error_type} file {file_name}')
+    print(f'Error while {error_type} file {file_name}')
 
 # -----------------
-def logit(fn):
-    @wraps(fn)
-    def wrapped(*args, **kwargs):
-        global log_file_name
-        # with open(log_file_name, 'a') as logger:
-            # with contextlib.redirect_stderr(logger):
-        return fn(*args, **kwargs)
-    return wrapped
-
-@logit
-def parseFile(file_path):
+def parseFile(file_path: Path):
     # seems there is some cache mechanism in music21. Ensure file is reloaded.
-    Path(file_path).touch()
-    return converter.parse(file_path)
-
-@logit
-def writeFile(m21_converter, file_path, dest_dir):
-    file_name = file_path.split('/')[-1]
-    new_file = dest_dir + '/'
-    new_file += file_name.replace("nwc", "musicxml")
-    m21_converter.write(fmt="musicxml", fp=new_file)
+    file_path.touch()
+    # configure music21
+    instrument._ID = 0
+    return converter.parse(str(file_path))
 
 # -----------------
-errors = 0
-for current_file in original_files:
-    print(f"processing {current_file}", end='\r', flush=True)
-    try:
-        current_file = os.path.abspath(current_file)
-        instrument._ID = 0
-        c = parseFile(current_file)
-    except Exception as e:
-        errors += 1
-        printAndLog('parsing', current_file, str(e))
-        continue
-    try:
-        writeFile(c, current_file, dest_dir)
-    except Exception as e:
-        errors += 1
-        printAndLog('writing', new_file, str(e))
-        raise e
-        continue
+def writeFile(m21_converter, file_path: Path):
+    m21_converter.write(fmt="musicxml", fp=str(file_path))
 
-    sys.stdout.write("\033[K")
+# -----------------
+def convertFiles(original_files, destination_dir_path):
+    errors = 0
+    for current_file_path in original_files:
+        output_path = Path(destination_dir_path,  Path(current_file_path.name)).with_suffix('.musicxml')
 
-if errors > 0:
-    formatted_errors = '\033[39;1m' + str(errors) + '\033[39;22m'
-    print(f'terminated with {formatted_errors} error(s)')
-    sys.exit(1)
+        path_as_string = str(current_file_path)
+        print(f"processing {path_as_string}", end='\r', flush=True)
 
-print("finished")
+        try:
+            c = parseFile(current_file_path)
+        except Exception as e:
+            errors += 1
+            printAndLog('parsing', path_as_string, str(e))
+            continue
+
+        try:
+            writeFile(c, output_path)
+        except Exception as e:
+            errors += 1
+            printAndLog('writing', str(output_path), str(e))
+            raise e
+            continue
+
+        # overwrite last line
+        sys.stdout.write("\033[K")
+    return errors
+
+# -----------------
+def parseArguments():
+    parser = argparse.ArgumentParser(description='NWC to musicxml converter')
+    parser.add_argument('source_files', nargs='+', help='the files to parse.')
+    parser.add_argument('destination_dir', help='Existing output directory')
+    #parser.add_argument('-l', '--log', action='store_true')
+
+    args = parser.parse_args()
+
+    args.source_files.sort()
+    original_pathes = [Path(x).resolve() for x in args.source_files]
+    destination_dir_path = Path(args.destination_dir).resolve()
+
+    return original_pathes, destination_dir_path
+
+
+# -----------------
+def finalize(nb_errors):
+    print("finished")
+
+    if errors > 0:
+        print(f'terminated with {errors} error(s)')
+
+
+if __name__ == '__main__':
+    original_files, destination_dir = parseArguments()
+
+    errors = convertFiles(original_files, destination_dir)
+
+    finalize(errors)
